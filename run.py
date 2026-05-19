5d84a335639b52a804f7aa45d6411fff1ce0ad25
Parthiangold <nathfann01@gmail.com>
2026-05-19 12:32:06 +1000

    Add comments to run.py for clarity
    Added comments to clarify the purpose of the code.

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import createApp
 
+# Invokes the createApp function which creates the localhost server for debug testing
 app = createApp()
 
+# Runs the localhost server 
 if __name__ == "__main__":
     app.run(debug=True)
