7bbc779e5b71f1fe9b48009c20d0180c714f07a7
Parthiangold <nathfann01@gmail.com>
2026-05-27 15:20:28 +1000

    models are now ready for use as a package

---
+++ app/models/__init__.py
@@ -0,0 +1,9 @@
+from .user import User
+from .seeker import SeekerProfile
+from .employer import EmployerProfile
+from .skills import Skill
+from .jobPosting import JobPosting
+from .jobApplication import JobApplication
+from .workExp import WorkExperience
+
+from .skills import seekerSkills, jobSkills
